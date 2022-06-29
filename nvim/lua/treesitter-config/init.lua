37d4027a2670d751d58146331b9dc1e8e9916a4e
Khang Tran <khangtrann8198@gmail.com>
2022-06-29 23:22:06 +0700

    Update rainbow color config

--- nvim/lua/treesitter-config/init.lua
+++ nvim/lua/treesitter-config/init.lua
@@ -24,8 +24,24 @@ require 'nvim-treesitter.configs'.setup {
     disable = { "html" }, -- list of languages you want to disable the plugin for
     extended_mode = false, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
     max_file_lines = nil, -- Do not enable for files with more than n lines, int
-    -- colors = {}, -- table of hex strings
-    -- termcolors = {} -- table of colour name strings
+    colors = {
+      "#ff79c6",
+      "#a89984",
+      "#b16286",
+      "#d79921",
+      "#689d6a",
+      "#ff79c6",
+      "#458588",
+    },
+    termcolors = {
+      "Magenta",
+      "Green",
+      "Yellow",
+      "Blue",
+      "Magenta",
+      "Cyan",
+      "White",
+    },
   },
   autopairs = {
     enable = true
